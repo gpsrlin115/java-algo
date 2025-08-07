a0b10f12b67ea021566370089b90a0293ee0a954
sjkim <SSAFY@DESKTOP-J3E0V2V>
2025-08-07 17:49:18 +0900

    countIsland temp

---
+++ src/main/java/org/countIsland/countIsland4963.java
@@ -0,0 +1,38 @@
+package org.countIsland;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class countIsland4963 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] str = br.readLine().split(" ");
+        int w = Integer.parseInt(str[0]);
+        int h = Integer.parseInt(str[1]);
+
+        int[][] arr = new int[w][h];
+
+        for (int i = 0; i<w; i++){
+            String tempStr = br.readLine();
+            String[] nums = tempStr.split(" ");
+            for (int j = 0; j<h; j++){
+                arr[i][j] = Integer.parseInt(nums[j]);
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 0; i<w; i++){
+            for(int j = 0; j<h; j++){
+                
+            }
+        }
+
+    }
+}
