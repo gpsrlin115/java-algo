7eed36b08b2a2410b73070f552b8f07ec2720d6c
gpsrlin115 <lefd123@gmail.com>
2025-08-10 01:59:37 +0900

    fix : twodots complete feat: koori5578 complete

--- src/main/java/org/example/twodots/twodots.java
+++ src/main/java/org/example/twodots/twodots.java
@@ -1,7 +1,71 @@
 package org.example.twodots;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class twodots {
     //brute force로 하되
     //탐색은 DFS로 하기.
     //빡구현 문제 ㅇㅇ..
+    static boolean[][] visited;
+    static int[] dr = {-1,1,0,0}; //상 하 좌 우
+    static int[] dc = {0,0,-1,1}; //상 하 좌 우
+    static String[] grid;
+    static boolean dfs(int x, int y,int px, int py, char color, int N, int M) {
+        visited[x][y] = true;// 방문부터.
+
+        for (int i = 0; i<4; i++){
+            int nx = x + dr[i];
+            int ny = y + dc[i];
+            if ( 0<=nx && nx<N && 0<=ny && ny<M){ // 같은 색깔 체크
+                if (grid[nx].charAt(ny) == color){
+                    //바로 직전에 온 곳이 아닐때
+                    if (nx != px || ny != py){
+                        //이미 방문한곳 사이클 확인
+                        if(visited[nx][ny]){
+                            return true;
+                        }
+                        //방문한 곳이 아니면 계속
+                        if (dfs(nx, ny, x, y, color, N, M)){
+                            return true;
+                        };
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new  StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        grid = new String[N];
+        for(int i=0;i<N;i++){
+            grid[i] = br.readLine();
+        }
+        visited = new boolean[N][M];
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                if(!visited[i][j]){
+                    if (dfs(i,j,i+1,j+1,grid[i].charAt(j), N, M)){
+                        bw.write("Yes");
+                        bw.flush();
+                        bw.close();
+                        br.close();
+                        return;
+                    }
+                }
+            }
+        }
+        bw.write("No");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
---
+++ src/main/java/org/example/usukooriwatari/koori5578.java
@@ -0,0 +1,67 @@
+package org.example.usukooriwatari;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class koori5578 {
+    static int N, M;
+    static int[][] grid;
+    static boolean[][] visited;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+
+    static int DFS (int x, int y){
+        visited[x][y] = true;
+        int count = 1;// 현재부터
+        for(int i = 0; i<N; i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            for(int j = 0; j<M; j++){
+                if(nx>=0 && nx <N && ny>=0 && ny<M){
+                    //얼음이 있고 아직 방문 x
+                    if(grid[nx][ny] == 1 && !visited[nx][ny]){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        M = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+
+        grid = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int maxArea = 0;
+
+        //모든위치 완전 탐색
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                //얼음이 있고 아직 방문 x
+                int currentArea = DFS(i,j);
+                maxArea = Math.max(maxArea, currentArea);
+            }
+        }
+        bw.write(String.valueOf(maxArea)+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
