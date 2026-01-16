8d85d87982139f7b6f2cda0c343ac1799aac3bd7
gpsrlin115 <lefd123@gmail.com>
2026-01-17 00:03:12 +0900

    cc

--- src/main/java/org/example/lab2/BOJ17141.java
+++ src/main/java/org/example/lab2/BOJ17141.java
@@ -1,16 +1,33 @@
 package org.example.lab2;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ17141 {
     static int[] dr = {-1,1,0,0};
     static int[] dc = {0,0,-1,1};
-    static int n;
+    static int n,m;
     static int answerTime;
-    static int[][] graph;
+    static int[][] map;
     static boolean[][] visited;
-
+    /*
+    0 빈칸
+    1 벽
+    2 바이러스를 놓을 수 있는 칸
+    */
     public static void main(String[] args) throws IOException {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][n];
+        for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<n; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
     }
 }
